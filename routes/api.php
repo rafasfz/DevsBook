d0b991bb9fdec1e5a9ffccc6eea84e7588142447
Rafael <rafaelsilvafreire2@gmail.com>
2022-02-20 19:03:39 -0300

    registred routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/ping', function() {
     return ['pong' => true];
 });
+
+Route::get('/401', 'AuthController@unauthorized')->name('login');
+
+Route::post('/auth/login', 'AuthController@login');
+Route::post('/auth/logout', 'AuthController@logout');
+Route::post('/auth/refresh', 'AuthController@refresh');
+
+Route::post('/user', 'AuthController@create');
+Route::put('/user', 'UserController@update');
+Route::post('/user/avatar/', 'UserController@updateAvatar');
+Route::post('/user/cover/', 'UserController@updateCover');
+
+Route::get('/feed', 'FeedController@read');
+Route::get('/user/feed', 'FeedController@userFeed');
+Route::get('/user/{id}/feed', 'FeedController@userFeed');
+
+Route::get('/user', 'UserController@read');
+Route::get('/user/{id}', 'UserController@read');
+
+Route::post('/feed', 'FeedController@create');
+
+Route::post('/post/{id}/like', 'PostController@like');
+Route::post('/post/{id}/comment', 'PostController@comment');
+
+Route::get('/search', 'SearchController@search');
