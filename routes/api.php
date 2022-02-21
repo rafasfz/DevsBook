64b7ba4895639a3ff5c1d95859e20c9b0ce3a73e
Rafael <rafaelsilvafreire2@gmail.com>
2022-02-21 19:29:54 -0300

    commented all the routes that is unfinished

--- routes/api.php
+++ routes/api.php
@@ -18,27 +18,27 @@ Route::get('/ping', function() {
     return ['pong' => true];
 });
 
-Route::get('/401', 'AuthController@unauthorized')->name('login');
+// Route::get('/401', 'AuthController@unauthorized')->name('login');
 
-Route::post('/auth/login', 'AuthController@login');
-Route::post('/auth/logout', 'AuthController@logout');
-Route::post('/auth/refresh', 'AuthController@refresh');
+// Route::post('/auth/login', 'AuthController@login');
+// Route::post('/auth/logout', 'AuthController@logout');
+// Route::post('/auth/refresh', 'AuthController@refresh');
 
-Route::post('/user', 'AuthController@create');
-Route::put('/user', 'UserController@update');
-Route::post('/user/avatar/', 'UserController@updateAvatar');
-Route::post('/user/cover/', 'UserController@updateCover');
+// Route::post('/user', 'AuthController@create');
+// Route::put('/user', 'UserController@update');
+// Route::post('/user/avatar/', 'UserController@updateAvatar');
+// Route::post('/user/cover/', 'UserController@updateCover');
 
-Route::get('/feed', 'FeedController@read');
-Route::get('/user/feed', 'FeedController@userFeed');
-Route::get('/user/{id}/feed', 'FeedController@userFeed');
+// Route::get('/feed', 'FeedController@read');
+// Route::get('/user/feed', 'FeedController@userFeed');
+// Route::get('/user/{id}/feed', 'FeedController@userFeed');
 
-Route::get('/user', 'UserController@read');
-Route::get('/user/{id}', 'UserController@read');
+// Route::get('/user', 'UserController@read');
+// Route::get('/user/{id}', 'UserController@read');
 
-Route::post('/feed', 'FeedController@create');
+// Route::post('/feed', 'FeedController@create');
 
-Route::post('/post/{id}/like', 'PostController@like');
-Route::post('/post/{id}/comment', 'PostController@comment');
+// Route::post('/post/{id}/like', 'PostController@like');
+// Route::post('/post/{id}/comment', 'PostController@comment');
 
-Route::get('/search', 'SearchController@search');
+// Route::get('/search', 'SearchController@search');
